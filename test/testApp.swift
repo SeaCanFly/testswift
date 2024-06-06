4d63ce76739506cffbc2f044e6a5e7766f55d12f
seaacanfly <seacanfly@gmail.com>
2024-06-06 10:03:09 +0900

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by seacanfly on 2024/06/06.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
